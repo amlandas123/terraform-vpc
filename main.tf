d24dc50015e98e2c835901c2beb0bcacdfaa4505
Amlan Das <dasamlan271988@gmail.com>
2024-01-04 18:48:33 +0530

    From amlan

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
-  source        = "git::https://github.com/amlandas123/tf-module-vpc"
-  # source        = "./vendor/modules/vpc"
+  #source        = "git::https://github.com/amlandas123/tf-module-vpc"
+  source        = "./vendor/modules/vpc"
   ENV           = var.ENV
   VPC_CIDR      = var.VPC_CIDR
   public_subnet_cidr   = var.public_subnet_cidr
