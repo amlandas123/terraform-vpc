4356f2645ba39a7fe454dcb9baeb49dfb7f8e0dc
Amlan Das <dasamlan271988@gmail.com>
2024-01-05 06:03:24 +0530

    From amlan

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ module "vpc" {
     source                = "./vendor/modules/vpc"
     ENV                   = var.ENV
     VPC_CIDR              = var.VPC_CIDR
-    PUBLIC_SUBNET_CIDR    = var.PUBLIC_SUBNET_CIDR
-    PRIVATE_SUBNET_CIDR   = var.PRIVATE_SUBNET_CIDR
+    # PUBLIC_SUBNET_CIDR    = var.PUBLIC_SUBNET_CIDR
+    # PRIVATE_SUBNET_CIDR   = var.PRIVATE_SUBNET_CIDR
 }
